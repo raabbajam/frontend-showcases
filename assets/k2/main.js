8811a338f7f72629c913e729e4042cc679b7dbd0
Muhammad Abdul Jabbaar <raabbajam@gmail.com>
2015-06-19 16:51:55 +0700

    Added smooth scroll on k2

--- assets/k2/main.js
+++ assets/k2/main.js
@@ -1,10 +1,17 @@
 $(function () {
-  function toggleLogo () {
-    $('.logo').toggleClass('invert');
-  };
 
   var $content = $('.content');
   var $itemWrapper = $('.item-wrapper');
+
+  // get data
+  $.getJSON('./data/item.json', function (items) {
+    var items = items.map(function (item) {
+      return templates.item.render(item);
+    });
+    $itemWrapper.html($itemWrapper.html().trim() + items.join(''));
+    init();
+  });
+
   function init() {
 
     var $contentWidth = $content.width();
@@ -22,17 +29,19 @@ $(function () {
 
       // scroll handle
       .on('mousemove', function (e) {
+        // pos will be beetwen 0 - 1
         var pos = e.pageX / $contentWidth;
-        $itemWrapper.scrollLeft(pos * contentOverflow);
+
+        // scroll to min - max content width;
+        $itemWrapper.animate({scrollLeft:pos * contentOverflow}, {queue:false, duration: 300, easing: 'easeInOutCirc'});
       });
   }
 
-
-  $.getJSON('./data/item.json', function (items) {
-    var items = items.map(function (item) {
-      return templates.item.render(item);
-    });
-    $itemWrapper.html($itemWrapper.html().trim() + items.join(''));
-    init();
-  });
+  function toggleLogo () {
+    $('.logo').toggleClass('invert');
+  };
+  $.easing.easeInOutCirc = function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+		return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+	}
 });
